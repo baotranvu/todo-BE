91e82ca934a4a483c4dec53f3ae3de4e612afe9a
baotran <tranvubao195@gmail.com>
2025-02-03 21:57:45 +0700

    [task]: update task validate wwhen create or update

--- app/Http/Requests/UpdateTaskRequest.php
+++ app/Http/Requests/UpdateTaskRequest.php
@@ -26,7 +26,7 @@ class UpdateTaskRequest extends FormRequest
         return [
             'name' => 'required|string|max:255',
             'priority' => ['required', 'string', new Enum(TaskPriority::class)],
-            'description' => 'required|string|max:255',
+            'description' => 'nullable|string|max:255',
             'progress' => 'required|integer|between:0,100',
             'start_date' => 'required|date|before_or_equal:due_date',
             'due_date' => 'required|date|after_or_equal:start_date',
